29d257f96840e6e218c7112420e7cca0e3c876b6
Luan <at.luan2915@gmail.com>
2025-10-08 22:29:31 -0300

    Regra de localização

---
+++ src/main/java/com/devsDoAgi/SAFeR/fraudes/rules/emAnalise/RuleLocation.java
@@ -0,0 +1,94 @@
+package com.devsDoAgi.SAFeR.fraudes.rules.emAnalise;
+import com.devsDoAgi.SAFeR.fraudes.engine.FraudResult;
+import com.devsDoAgi.SAFeR.fraudes.interfaces.FraudRule;
+import com.devsDoAgi.SAFeR.model.Transacao;
+import com.devsDoAgi.SAFeR.repository.TransacaoRepository;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.locationtech.spatial4j.context.SpatialContext;
+import org.locationtech.spatial4j.distance.DistanceUtils;
+import org.locationtech.spatial4j.shape.Point;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.time.Duration;
+import java.util.Optional;
+
+@Service
+@Data
+@AllArgsConstructor
+public class RuleLocation implements FraudRule {
+
+    @Autowired
+    TransacaoRepository transacaoRepository;
+    SpatialContext ctx = SpatialContext.GEO;
+
+    private Double calcDistanceInKm(Transacao currentTrasaction, Transacao lastTransaction) { //A transação anterior será inserida no construtor diretamente, a validaçção do get será feita no generalista da classe
+
+        Double currentlongitude = currentTrasaction.getLocal()[0];
+        Double currentLatitude = currentTrasaction.getLocal()[1];
+
+        Double lastLongitude = lastTransaction.getLocal()[0];
+        Double lastLatitude = lastTransaction.getLocal()[1];
+
+        //Criação de objetos para calculo de distancia entre pontos
+        Point currentLocation = ctx.getShapeFactory().pointXY(currentlongitude, currentLatitude);
+        Point lastLocation = ctx.getShapeFactory().pointXY(lastLongitude, lastLatitude);
+
+        //Faz o calculo da distancia dos dois pontos em graus e depois utiliza a constante DEG_TO_KM para converção em KM
+        Double distance = ctx.calcDistance(currentLocation, lastLocation) * DistanceUtils.DEG_TO_KM;
+
+        return distance;
+    }
+
+    //Metodo que calcula o tempo minimo aceitável entre transações em diferentes localidades
+    private Duration calcMinimumTime(Double distanceInKm) {
+
+        double hours = distanceInKm / 800.0; // velocidade média de um voo comercial
+        int hourPart = (int) hours;
+        int minutePart = (int) ((hours - hourPart) * 60);
+
+        return Duration.ofHours(hourPart).plusMinutes(minutePart);
+
+    }
+
+    @Override
+    public FraudResult evaluate(Transacao currentTransaction) {
+
+        Duration maxAcceptablePeriod = Duration.ofHours(6); //Determina o periodo maximo entre transações para ativação da regra
+        Optional<Transacao> lastTransactionOptional = transacaoRepository.getLastTransaction();
+
+        if (lastTransactionOptional.isPresent()) { //Verifica se há transações anteriores
+
+            Transacao lastTransaction = lastTransactionOptional.get();
+
+            Double currentlongitude = currentTransaction.getLocal()[0];
+            Double currentLatitude = currentTransaction.getLocal()[1];
+
+            Double lastLongitude = lastTransaction.getLocal()[0];
+            Double lastLatitude = lastTransaction.getLocal()[1];
+
+            if (currentlongitude != lastLongitude && currentLatitude != lastLatitude) {
+                // Tempo entre ultima transação e atual
+                Duration periodBtwTransaction = Duration.between(lastTransaction.getDataHoraOperacao(), currentTransaction.getDataHoraOperacao());
+
+                // Compara a duração entre transações com o ideal
+                // Se o resultado for negativo ou igual a zero signififica que o periodo entre transações é classificável para ativação da regra
+                if (periodBtwTransaction.compareTo(maxAcceptablePeriod) <= 0) { //Verifica se transações acontecem em locais diferentes
+                    Double distanceInKm = calcDistanceInKm(currentTransaction, lastTransaction);  //Calculo da distancia entre as duas localizações
+                    Duration minimumTime = calcMinimumTime(distanceInKm);
+
+                    // Se resultado for negativo, a transação é considerada impossivel
+                    if (periodBtwTransaction.compareTo(minimumTime) <= 0) {
+
+                        return new FraudResult("Regra de localização", 90); //Alta chance de ser fraude
+                    } else {
+                        return new FraudResult("Regra de localização", 0);
+                    }
+                }
+            }
+
+        }
+        return new FraudResult("Regra de localização", 0);
+    }
+}
--- src/main/java/com/devsDoAgi/SAFeR/repository/TransacaoRepository.java
+++ src/main/java/com/devsDoAgi/SAFeR/repository/TransacaoRepository.java
@@ -2,6 +2,12 @@ package com.devsDoAgi.SAFeR.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.devsDoAgi.SAFeR.model.Transacao;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface TransacaoRepository extends JpaRepository<Transacao, Long> {
+
+    @Query(value = "select * from transacoes order by data_hora_operacao desc limit 1",nativeQuery = true)
+    public Optional<Transacao> getLastTransaction();
 }
